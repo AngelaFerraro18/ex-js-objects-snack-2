3062bbe03c48a454403317feec6986f7e0077760
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-06-26 16:15:52 +0200

    feat: completed snacks n* 5 and 6

--- app.js
+++ app.js
@@ -75,3 +75,59 @@ const restaurant = {
 // Per clonare l'oggetto chef userei lo spread operator perchè esso ci permette, insieme alla reference, di clonare anche le funzioni presenti nell'oggetto
 
 // Per clonare l'oggetto restaurant userei invece la modalità con structuredClone  perchè non solo abbiamo degli oggetti annidati ma abbiamo anche il metodo Date, in questo modo la data verrà clonata nel formato corretto
+
+
+/* Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?*/
+
+const hamburgerrr = {
+    name: "Cheese Burger",
+    weight: 250,
+    maker: {
+        name: "Anonymous Chef",
+        restaurant: {
+            name: "Hyur's Burgers",
+            address: "Main Street, 123",
+            isOpen: true,
+        },
+        age: 29
+    }
+};
+
+const newRestaurant = { ...hamburgerrr.maker.restaurant };
+newRestaurant.name = "Hyur's II";
+newRestaurant.address = "Second Street, 12";
+const secondBurgerrr = { ...hamburgerrr };
+secondBurgerrr.maker.restaurant = newRestaurant;
+secondBurgerrr.maker.name = "Chef Hyur";
+
+console.log(hamburgerrr.maker.name); // "Chef Hyur"
+console.log(secondBurgerrr.maker.name); // "Chef Hyur"
+console.log(hamburgerrr.maker.restaurant.name); // "Hyur's II"
+console.log(secondBurgerrr.maker.restaurant.name); // "Hyur's II"
+
+/* Qual è il metodo migliore per clonare l’oggetto chef, e perché?*/
+
+const cheff = {
+    name: "Chef Hyur",
+    age: 29,
+    makeBurger: (num = 1) => {
+        console.log(`Ecco ${num} hamburger per te!`);
+    },
+    restaurant: {
+        name: "Hyur's Burgers",
+        welcomeClient: () => {
+            console.log("Benvenuto!");
+        },
+        address: {
+            street: 'Main Street',
+            number: 123,
+            showAddress: () => {
+                console.log("Main Street 123");
+            }
+        },
+        isOpen: true,
+    }
+}
+
+// Userei lo spread operator perchè mi permette di clonare anche le funzioni
